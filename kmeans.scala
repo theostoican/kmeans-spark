b41ea4a9f07eaf98728dcef73766282ea142f835
Theodor Stoican <theo.stoican@gmail.com>
2019-01-07 13:45:24 +0200

    Finished plot for kmeans result

---
+++ kmeans.scala
@@ -0,0 +1,23 @@
+import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
+import org.apache.spark.mllib.linalg.Vectors
+
+val r_csv = sc.textFile("gs://bigdata-iris/Iris.csv")
+val header = r_csv.first()
+val r_csv2 = r_csv.filter(row => row != header)
+val t = r_csv2.map(line=> line.split(',')(1) + "," + line.split(',')(2) + "," + line.split(',')(3) + "," + line.split(',')(4))
+val convData = t.map(s => Vectors.dense(s.split(',').map(_.toDouble))).cache()
+
+val numClusters = 3
+val numIterations = 20
+val clusters = KMeans.train(convData, numClusters, numIterations)
+val WSSSE = clusters.computeCost(convData)
+
+
+val foo = clusters.predict(convData)
+
+// Save labels
+foo.saveAsTextFile("gs://bigdata-iris/labels")
+
+// Save original features
+val convDataArr = convData.map(arr => arr.toArray)
+convDataArr.map(arr => arr(0) + "," + arr(1) + "," + arr(2) + "," + arr(3)).saveAsTextFile("gs://bigdata-iris/data")
